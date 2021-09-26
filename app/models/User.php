4f195246b478933d5abf0f46a8463aac2567b358
nimeshnishamal <62231124+nimeshnishamal@users.noreply.github.com>
2021-09-26 20:18:25 +0530

    documentation changed

--- app/models/User.php
+++ app/models/User.php
@@ -6,13 +6,4 @@
             $this->db = new Database;
         }
 
-        /* Test (database and table needs to exist before this works)
-        public function getUsers() {
-            $this->db->query("SELECT * FROM users");
-
-            $result = $this->db->resultSet();
-
-            return $result;
-        }
-        */
     }
